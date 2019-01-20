89deb648d02cf4f8cbc37cca9c22c250bdbf7fab
imcpp <chandanprakash1122@gmail.com>
2019-01-20 10:18:24 +0530

    second commit

--- app/views.py
+++ app/views.py
@@ -8,12 +8,13 @@ from  django.contrib import messages,auth
 from django.http import HttpResponseRedirect
 import requests
 from bs4 import BeautifulSoup
+import random
 #from selenium import webdriver
 class cp:
     def __init__(self,course,review,rating,price,website):
         self.course=course
         self.review=review
-        self.rating=rating
+        self.rating=round(random.uniform(2.3,5),1)
         self.price=price
         self.website=website
 
@@ -27,24 +28,33 @@ def search(request):
     if request.method=='POST':
         search=request.POST['srh']
 
+
         if search:
              URL = "https://www.edureka.co/search/"+search
+             URL2= "https://www.simpliv.com"+search
              r = requests.get(URL)
              soup = BeautifulSoup(r.text,'lxml')
+             r2 = requests.get(URL)
+             soup2 = BeautifulSoup(r2.text,'lxml')
+             soup2.prettify()
              soup.prettify()
              event_containers =(soup.find_all('div',class_ = "textinfoleft"))
              reviews = soup.find_all('span',attrs = {'class':'totalreviews'})
              ratings  = soup.find_all('span',attrs = {'class':'rating'})
              prices = soup.find_all('div',class_ = "pricemain")
+             names = soup.find_all('div',class_ = "course-desc")
              courses=[]
              review=[]
              rating=[]
              price=[]
              p=[]
              website=[]
+             j=0
              for i in event_containers:
                  courses.append(i.h3.text)
                  website.append(URL)
+
+
              for i in reviews:
                  review.append(i.text)
 
@@ -53,11 +63,13 @@ def search(request):
              for i in prices:
                  price.append(i.contents[0].text)
 
+
              for i in range(0,len(courses)):
                  p.append(cp(courses[i],review[i],rating[i],price[i],website[i]))
 
 
              return render(request,'home.html',{"all":sorted(p,key=lambda x: x.rating,reverse=True)})
+
 
         else:
             return HttpResponseRedirect('')
--- project1/urls.py
+++ project1/urls.py
@@ -26,12 +26,14 @@ urlpatterns = [
 from django.contrib import admin
 from django.urls import path
 from app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name='home'),
     path('search/',views.search,name='search'),
-   
 
 
-]
+
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
